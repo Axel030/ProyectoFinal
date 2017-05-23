db323a306f5b51f4bd76be45da3960afbc5d5e7f
Axel Fabián Avilá García <latente2010@hotmail.com>
2017-05-22 19:52:27 -0500

    listas creadas

--- proyectoprogra/proyectoprogra/Gastos.cs
+++ proyectoprogra/proyectoprogra/Gastos.cs
@@ -8,6 +8,47 @@ namespace proyectoprogra
 {
     class Gastos
     {
+        string identificacion;
+        string nombre;
+        string tipreparto;
 
+        public string Identificacion
+        {
+            get
+            {
+                return identificacion;
+            }
+
+            set
+            {
+                identificacion = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Tipreparto
+        {
+            get
+            {
+                return tipreparto;
+            }
+
+            set
+            {
+                tipreparto = value;
+            }
+        }
     }
 }
