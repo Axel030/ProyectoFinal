e287e4124ea87932582a0a8fac14f7d5997af1e7
Axel Fabián Avilá García <latente2010@hotmail.com>
2017-05-22 20:17:14 -0500

    clases terminadas1

---
+++ proyectoprogra/proyectoprogra/Gastos2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoprogra
+{
+    class Gastos2
+    {
+        string idgastos;
+        string descripcion;
+        int importe;
+        string zonadereparto;
+
+        public string Idgastos
+        {
+            get
+            {
+                return idgastos;
+            }
+
+            set
+            {
+                idgastos = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+
+            set
+            {
+                descripcion = value;
+            }
+        }
+
+        public int Importe
+        {
+            get
+            {
+                return importe;
+            }
+
+            set
+            {
+                importe = value;
+            }
+        }
+
+        public string Zonadereparto
+        {
+            get
+            {
+                return zonadereparto;
+            }
+
+            set
+            {
+                zonadereparto = value;
+            }
+        }
+    }
+}
---
+++ proyectoprogra/proyectoprogra/Propiedad.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectoprogra
+{
+    class Propiedad
+    {
+        string tiposdepropiedades;
+        string codigoidentificador;
+        int metroscuadrados;
+        string nit;
+        string listaporcentajes;
+
+        public string Tiposdepropiedades
+        {
+            get
+            {
+                return tiposdepropiedades;
+            }
+
+            set
+            {
+                tiposdepropiedades = value;
+            }
+        }
+
+        public string Codigoidentificador
+        {
+            get
+            {
+                return codigoidentificador;
+            }
+
+            set
+            {
+                codigoidentificador = value;
+            }
+        }
+
+        public int Metroscuadrados
+        {
+            get
+            {
+                return metroscuadrados;
+            }
+
+            set
+            {
+                metroscuadrados = value;
+            }
+        }
+
+        public string Nit
+        {
+            get
+            {
+                return nit;
+            }
+
+            set
+            {
+                nit = value;
+            }
+        }
+
+        public string Listaporcentajes
+        {
+            get
+            {
+                return listaporcentajes;
+            }
+
+            set
+            {
+                listaporcentajes = value;
+            }
+        }
+    }
+}
