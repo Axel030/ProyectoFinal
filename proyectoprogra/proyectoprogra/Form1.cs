cb1b83c5a852d3c2460cd606c717b0ac39ed1854
Axel Fabián Avilá García <latente2010@hotmail.com>
2017-05-25 17:26:31 -0500

    listas juntas

--- proyectoprogra/proyectoprogra/Form1.cs
+++ proyectoprogra/proyectoprogra/Form1.cs
@@ -23,7 +23,7 @@ namespace proyectoprogra
         List<Piso> listapiso = new List<Piso>();
         List<Garaje> listagaraje = new List<Garaje>();
 
-        public void cargando(bool a, bool b)
+        public void cargando(bool a, bool b, bool c, bool d, bool e)
         {
             if (a == true)
             {
@@ -58,6 +58,75 @@ namespace proyectoprogra
                     gastostemp.Nombre = _textStreamReader.ReadLine();
                     gastostemp.Tipreparto = _textStreamReader.ReadLine();
                     listagastos.Add(gastostemp);
+                }
+                _textStreamReader.Close();
+
+            }
+            if (c == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("proyectoprogra.DatosPropietarios.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("proyectoprogra.DatosPropietarios.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                {
+                    DatosPropietarios datospropietariostemp = new DatosPropietarios();
+                    datospropietariostemp.Nombre = _textStreamReader.ReadLine();
+                    datospropietariostemp.Nit = _textStreamReader.ReadLine();
+                    datospropietariostemp.Email =_textStreamReader.ReadLine();
+                    
+                    listadatospropietarios.Add(datospropietariostemp);
+                }
+                _textStreamReader.Close();
+
+            }
+            if (d == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("proyectoprogra.Gastos2.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("proyectoprogra.Gastos2.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                {
+                    Gastos2 gastos2temp = new Gastos2();
+                    gastos2temp.Idgastos = _textStreamReader.ReadLine();
+                    gastos2temp.Descripcion = _textStreamReader.ReadLine();
+                    gastos2temp.Importe = Convert.ToInt16(_textStreamReader.ReadLine());
+                    gastos2temp.Zonadereparto = _textStreamReader.ReadLine();
+                    listagastos2.Add(gastos2temp);
+                }
+                _textStreamReader.Close();
+            }
+            if ( e == true)
+            {
+                Assembly _assembly; Stream _imageStream;
+                StreamReader _textStreamReader;
+                _assembly = Assembly.GetExecutingAssembly();
+                _imageStream = _assembly.GetManifestResourceStream("proyectoprogra.Propiedad.bmp");
+                _textStreamReader = new StreamReader(_assembly.GetManifestResourceStream("proyectoprogra.Propiedad.txt"));
+
+                while (_textStreamReader.Peek() > -1)
+                { 
+                  
+                    if (_textStreamReader.ReadLine()== "L")
+                    {
+                        Localcomercial localcomerciatemp = new Localcomercial();
+                    }
+                    if (_textStreamReader.ReadLine() == "G")
+                    {
+                        Garaje garajetemp = new Garaje();
+                    }
+                    if (_textStreamReader.ReadLine() == "P")
+                    {
+                        Piso pisotemp = new Piso();
+                    }
+
+
+
                 }
                 _textStreamReader.Close();
 
